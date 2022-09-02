1f41c1eb699f3a1fd68ea936f2be138762183c91
Zhi Li <lzblack@users.noreply.github.com>
2022-09-01 20:06:50 -0400

    Finish session 02

--- calc.py
+++ calc.py
@@ -7,3 +7,9 @@ print(2022 - 2001)
 print(15 / 3)
 
 print(2 ** 5)
+
+# Ex02-1
+print(2 + 2)
+
+# Ex02-2
+print()
--- hello.py
+++ hello.py
@@ -1,7 +1,1 @@
-# print('Hello, world')
-
-# Ex02-1
-print(2 + 2)
-
-# Ex02-2
-print()
+print('Hello, world')
