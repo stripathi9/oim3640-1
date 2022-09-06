40329dd8ebb411a273c3e54595b0257d5061a885
Zhi Li <lzblack@users.noreply.github.com>
2022-09-06 13:43:51 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,15 +1,33 @@
-print(2 * 2)
+"""
+Ex. 2.1
+"""
+minutes = 42
+seconds = 42
 
-print(39 + 41 + 33)
-
-print(2022 - 2001)
-
-print(15 / 3)
-
-print(2 ** 5)
-
-# Ex02-1
-print(2 + 2)
-
-# Ex02-2
+print("The time duration is", minutes * 60 + seconds, "seconds.")
 print()
+
+"""
+Ex. 2.2
+"""
+km_per_mile = 1.61
+km = 10
+miles = km / km_per_mile
+print("There are", miles, "miles in", km, "kilometers.")
+print()
+
+"""
+Ex. 2.3
+"""
+minutes = 42 + 42 / 60
+hours = minutes / 60
+
+km_per_mile = 1.61
+km = 10
+miles = km / km_per_mile
+
+pace = minutes / miles
+mph = miles / hours
+
+print("Pace in minutes per mile:", pace)
+print("Average speed in mph:", mph)
