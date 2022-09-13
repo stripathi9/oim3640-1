6206bb4f625efa22cf6d40055771e61016a3908a
Zhi Li <lzblack@users.noreply.github.com>
2022-09-13 16:46:22 -0400

    Update ex_03_sol.py

--- session04/ex_03_sol.py
+++ session04/ex_03_sol.py
@@ -7,8 +7,8 @@ print(time.time())
 
 current = time.time()
 second = current % 60
-minutes = (current // 60) % 60
-hours = (current // 60) // 60 % 24
+minutes = current // 60 % 60
+hours = current // 60 // 60 % 24
 days = current // 60 // 60 // 24
 
 print(
