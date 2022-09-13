6fe455a89c186f4172fc24c085bbb4085dd512b7
Zhi Li <lzblack@users.noreply.github.com>
2022-09-13 18:25:21 -0400

    Update ex_03_sol.py

--- session04/ex_03_sol.py
+++ session04/ex_03_sol.py
@@ -6,11 +6,13 @@ import time
 print(time.time())
 
 current = time.time()
-second = current % 60
+seconds = current % 60
 minutes = current // 60 % 60
 hours = current // 60 // 60 % 24
 days = current // 60 // 60 // 24
 
+# print(seconds, minutes, hours, days)
+
 print(
-    f"Current time: {int(days):d} days, {int(hours):d} hours, {int(minutes):d} minutes and {second:.2f} seconds from Epoch."
+    f"Current time: {int(days):d} days, {int(hours):d} hours, {int(minutes):d} minutes and {seconds:.2f} seconds from Epoch."
 )
