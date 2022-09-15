184fd3102f93e5b9f9e25824826a0423948db3cf
Zhi Li <lzblack@users.noreply.github.com>
2022-09-15 18:29:28 -0400

    Create temp_01.py

---
+++ temp_01.py
@@ -0,0 +1,37 @@
+def f(x):
+    return x + 1, x * 2
+
+
+# y = f(10)
+# print(type(y))
+# y1, y2 = f(20)
+# print(y1)
+# print(y2)
+# print(x)
+
+
+def quadratic(a, b,c):
+    """
+    returns something
+    """
+    pass
+    # TODO: 
+
+help(quadratic)
+# quadratic(1, 2, 3)
+
+
+
+def f1():
+    
+    print('Hi') # reference: https://stackoverflow.com/a/1077349
+
+# result = f1()
+# print(result)
+
+
+def f2(x):
+    x = 10
+    return x * 2
+
+print(f2(43245324))
