a8d2aec53dfd970a82d20c3dad3f9273a8dcb6ca
Zhi Li <lzblack@users.noreply.github.com>
2022-09-22 16:34:33 -0400

    Add sol to session 07

---
+++ session07/exercises/greatest_common_divisor.py
@@ -0,0 +1,23 @@
+def gcd_recur(a, b):
+    """
+    a, b: positive integers
+
+    returns: a positive integer, the greatest common divisor of a & b.
+    """
+    # Your code here
+    print("Current a, b:", a, b)  # for testing
+    if b == 0:
+        return a
+    else:
+        return gcd_recur(b, a % b)
+
+
+def main():
+    # print(gcd_recur(2, 12))
+    # print(gcd_recur(6, 12))
+    print(gcd_recur(9, 12))
+    # print(gcd_recur(17, 12))
+
+
+if __name__ == "__main__":
+    main()
---
+++ session07/exercises/hanoi.py
@@ -0,0 +1,24 @@
+# http://www.python-course.eu/towers_of_hanoi.php
+
+
+def move(n, source, bridge, destination):
+    """
+    Use recursion to solve tower of Hanoi problem.
+    """
+    if n == 1:
+        print(f"{source} --> {destination}")
+    else:
+        # move n-1 plates from origin to bridge
+        move(n - 1, source, destination, bridge)
+        # move the largest plate from origin to destination
+        print(f"{source} --> {destination}")
+        # move n-1 plates from bridge to destination
+        move(n - 1, bridge, source, destination)
+
+
+def main():
+    move(6, "A", "B", "C")
+
+
+if __name__ == "__main__":
+    main()
