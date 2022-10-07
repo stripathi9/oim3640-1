e4035ab9ea6912e64a784b29ef136ef677527ad2
Zhi Li <lzblack@users.noreply.github.com>
2022-10-06 20:12:25 -0400

    Finish session 12

---
+++ session12/is_reverse.py
@@ -0,0 +1,19 @@
+def is_reverse(word1, word2):
+    """Return True if WORD2 is the reversed WORD1"""
+    # if len(word1) != len(word2):
+    #     return False
+
+    # i = 0
+    # j = len(word2)
+
+    # while j > 0:
+    #     if word1[i] != word2[j]:
+    #         return False
+    #     i = i + 1
+    #     j = j - 1
+    # return True
+
+    return word1[::-1] == word2
+
+
+print(is_reverse('pots', 'stop'))
