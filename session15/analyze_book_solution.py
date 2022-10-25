05db36d98c4d6c17cd9dff1232072215fe6548f5
Zhi Li <lzblack@users.noreply.github.com>
2022-10-25 11:11:48 -0400

    Create analyze_book_solution.py

---
+++ session15/analyze_book_solution.py
@@ -0,0 +1,155 @@
+import random
+import string
+import sys
+from unicodedata import category
+
+
+def process_file(filename, skip_header):
+    """Makes a histogram that contains the words from a file.
+
+    filename: string
+    skip_header: boolean, whether to skip the Gutenberg header
+
+    returns: map from each word to the number of times it appears.
+    """
+    hist = {}
+    fp = open(filename, encoding='utf8')
+
+    if skip_header:
+        skip_gutenberg_header(fp)
+
+    # strippables = string.punctuation + string.whitespace
+    # via: https://stackoverflow.com/questions/60983836/complete-set-of-punctuation-marks-for-python-not-just-ascii
+
+    strippables = ''.join(
+        [chr(i) for i in range(sys.maxunicode) if category(chr(i)).startswith("P")]
+    )
+
+    for line in fp:
+        if line.startswith('*** END OF THIS PROJECT'):
+            break
+
+        line = line.replace('-', ' ')
+        line = line.replace(
+            chr(8212), ' '
+        )  # Unicode 8212 is the HTML decimal entity for em dash
+
+        for word in line.split():
+            # remove punctuation and convert to lowercase
+            word = word.strip(strippables)
+            word = word.lower()
+
+            # update the histogram
+            hist[word] = hist.get(word, 0) + 1
+
+    return hist
+
+
+def skip_gutenberg_header(fp):
+    """Reads from fp until it finds the line that ends the header.
+
+    fp: open file object
+    """
+    for line in fp:
+        if line.startswith('*** START OF THIS PROJECT'):
+            break
+
+
+def total_words(hist):
+    """Returns the total of the frequencies in a histogram."""
+    return sum(hist.values())
+
+
+def different_words(hist):
+    """Returns the number of different words in a histogram."""
+    return len(hist)
+
+
+def most_common(hist, excluding_stopwords=True):
+    """Makes a list of word-freq pairs(tuples) in descending order of frequency.
+
+    hist: map from word to frequency
+    excluding_stopwords: a boolean value. If it is True, do not include any stopwords in the list.
+
+    returns: list of (frequency, word) pairs
+    """
+    t = []
+
+    stopwords = process_file('data/stopwords.txt', False)
+
+    stopwords = list(stopwords.keys())
+    # print(stopwords)
+
+    for word, freq in hist.items():
+        if excluding_stopwords:
+            if word in stopwords:
+                continue
+
+        t.append((freq, word))
+
+    t.sort(reverse=True)
+    return t
+
+
+def print_most_common(hist, num=10):
+    """Prints the most commons words in a histgram and their frequencies.
+
+    hist: histogram (map from word to frequency)
+    num: number of words to print
+    """
+    t = most_common(hist)
+    print('The most common words are:')
+    for freq, word in t[:num]:
+        print(word, '\t', freq)
+
+
+def subtract(d1, d2):
+    """Returns a dictionary with all keys that appear in d1 but not d2.
+
+    d1, d2: dictionaries
+    """
+    res = {}
+    for key in d1:
+        if key not in d2:
+            res[key] = None
+    return res
+
+
+def random_word(hist):
+    """Chooses a random word from a histogram.
+
+    The probability of each word is proportional to its frequency.
+    """
+    t = []
+    for word, freq in hist.items():
+        t.extend([word] * freq)
+
+    return random.choice(t)
+
+
+def main():
+    hist = process_file('data/Pride and Prejudice.txt', skip_header=True)
+    print('Total number of words:', total_words(hist))
+    print('Number of different words:', different_words(hist))
+
+    t = most_common(hist, False)
+
+    print('The most common words are:')
+    for freq, word in t[0:20]:
+        print(word, '\t', freq)
+    print_most_common(hist, 20)
+
+    words = process_file('data/words.txt', skip_header=False)
+
+    diff = subtract(hist, words)
+    print("The words in the book that aren't in the word list are:")
+    for word in diff.keys():
+        print(word, end=' ')
+
+    print("\n\nHere are some random words from the book")
+    for i in range(100):
+        print(random_word(hist), end=' ')
+
+
+if __name__ == '__main__':
+    main()
